5a2131e1fd4894a64ce7b4034197d21d82135bf0
Thanh <thanhvkgdh190702@fpt.edu.vn>
2022-12-18 16:39:58 +0700

    1

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace demo_1670_book.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
